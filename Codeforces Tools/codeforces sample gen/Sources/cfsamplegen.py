a5cdaec35feb316d2d31de048297864941f50545
dotrann1412 <dotrann.1412@gmail.com>
2022-06-17 19:41:07 +0700

    create template with problem statement

--- Codeforces Tools/codeforces sample gen/Sources/cfsamplegen.py
+++ Codeforces Tools/codeforces sample gen/Sources/cfsamplegen.py
@@ -1,10 +1,15 @@
 import requests, json
 import os, sys, time
+import pdfkit 
+import threading
+# import lxml.html
 
 if(len(sys.argv) <= 1):
 	sys.exit("Contest ID not found")
 
-url = 'https://codeforces.com/api/contest.standings?contestId=' + str(sys.argv[1]) + '&from=1&count=1'
+contest_id = sys.argv[1]
+
+url = 'https://codeforces.com/api/contest.standings?contestId=' + str(contest_id) + '&from=1&count=1'
 
 #the idea is call the codeforces api and recieve the json data of score board
 #this data include the info abt contest and score board, we dont card abt score board
@@ -15,7 +20,7 @@ response_data_json = json.loads(response_data.text)
 if(response_data_json['status'] != 'OK'):
 	sys.exit(response_data_json['comment'])
 
-contest_name = response_data_json['result']['contest']['name']
+contest_name = response_data_json['result']['contest']['name'].strip().replace('\n', '').replace('\r', '')
 
 #split contest problems index from result
 problems_index_list = [problem['index'] for problem in response_data_json['result']['problems']]
@@ -25,10 +30,15 @@ print("Started time: " + time.ctime(response_data_json['result']['contest']['sta
 print("Problems list: " + str(problems_index_list))
 
 #change the default contest directory
-contest_directory = '/mnt/w/Coding/Contest/Codeforces/' + contest_name
+contest_directory = f'/mnt/e/Coding/Contest/CodeForces/{contest_name}'
+# contest_directory_w = f'E:/Coding/Contest/CodeForces/{contest_name}'
 
-if(os.path.exists(contest_directory) == False):
-	os.mkdir(contest_directory)
+if(os.path.exists(contest_directory) == True and not '-f' in sys.argv):
+	os.system("subl.exe \"E:" + contest_directory[6:len(contest_directory)] + "\"")
+	exit(0)
+
+try: os.mkdir(contest_directory)
+except: pass
 
 template_signature = """/*
  * Author : DoTran
@@ -37,24 +47,40 @@ template_signature = """/*
 
 """ %time.ctime(time.time())
 
-#if you need to create script file with template
-#create a cpp template file and change the path to the template file
-#or delete the next 4 lines if u dont need to create a script file with template
-template_path = "/mnt/w/Template/CPP Template/CPTemplate.cpp"
+# if you need to create script file with template
+# create a cpp template file and change the path to the template file
+# or delete the next 4 lines if u dont need to create a script file with template
+template_path = "/mnt/e/Template/CPP Template/CPTemplate.cpp"
 
-template_ostream = open(template_path, 'r')
-str_template = template_ostream.read()
-template_ostream.close()
+str_template = ""
 
-for problem in problems_index_list:
-	with open(contest_directory + "/" + problem + ".cpp", 'w') as problem_file:
+with open(template_path, 'r') as template_ostream:
+	str_template = template_ostream.read()
+
+# generate the in/out file at contest directory
+os.system("echo >\"" + contest_directory + "/inp.txt\"")
+os.system("echo >\"" + contest_directory + "/out.txt\"")
+
+# open contest folder with sublime text - change if you use a nother code editor or delete
+# if you dont want to use
+os.system("subl.exe \"E:" + contest_directory[6:len(contest_directory)] + "\"")
+
+def parse_problem(contest_id, problem):
+	pdf_path = f'{contest_directory}/{problem}.pdf'
+	cpp_path = f'{contest_directory}/{problem}.cpp'
+	
+	with open(cpp_path, 'w') as problem_file:
 		problem_file.write(template_signature)
 		problem_file.write(str_template)
 
-#generate the in/out file at contest directory
-os.system("echo >\"" + contest_directory + "/inp.txt\"") 
-os.system("echo >\"" + contest_directory + "/out.txt\"")
+	# print('[DEBUG] ', f'https://codeforces.com/contest/{contest_id}/problem/{problem}')
+	try: pass
+		pdfkit.from_url(f'https://codeforces.com/contest/{contest_id}/problem/{problem}', pdf_path)
+	except Exception as err: 
+		print(f'[ERROR][{contest_id}][{problem}]: ', err)
 
-#open contest folder with sublime text - change if you use a nother code editor or delete
-#if you dont want to use
-os.system("subl.exe \"W:" + contest_directory[6:len(contest_directory)] + "\"")
+# sudo strip --remove-section=.note.ABI-tag /usr/lib/x86_64-linux-gnu/libQt5Core.so.5
+for problem in problems_index_list:
+	threading.Thread(target = parse_problem, args = (contest_id, problem, )).start()
+
+pdfkit.from_url(f'https://codeforces.com/contest/{contest_id}/problems', f'{contest_directory}/problems.pdf')
